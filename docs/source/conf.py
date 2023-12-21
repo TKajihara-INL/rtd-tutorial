dbb1ae077e8568e7e4c7b4930ca411900cdd8b46
Takanori Kajihara <101886518+TKajihara-INL@users.noreply.github.com>
2023-12-21 12:43:11 -0700

    Update conf.py
    change project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ORCA'
+copyright = '2023, Idaho National Laboratory'
+author = ''
 
 release = '0.1'
 version = '0.1.0'
